a843aa4f160c8219410b186a777e055bf39b1080
Farzad DianatKhah <farzad.dianat@gmail.com>
2024-01-22 19:56:34 +0330

    feat: listeners for switching contents

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import createHome from "./homepage.js";
+import createMenu from "./menu.js";
 
-window.addEventListener("DOMContentLoaded", createHome);
+window.addEventListener("DOMContentLoaded", () => {
+  createHome();
+
+  const content = document.getElementById("content");
+  const main = document.querySelector("main");
+  const menu = document.querySelector(".menu");
+
+  menu.addEventListener("click", () => {
+    if (content.contains(main)) {
+      main.replaceWith(createMenu());
+    }
+  });
+});
