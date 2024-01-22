61b0180c58e7c451f93d4aa6e010e310cd9a7e88
Farzad DianatKhah <farzad.dianat@gmail.com>
2024-01-22 15:08:15 +0330

    Add content to homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,15 +1,27 @@
-import {
-  createHeaderWithNavigation,
-  createFooter,
-  createMain,
-} from "./components.js";
+import { div, p, main, section, img } from "./hypertext.js";
+import { createHeaderWithNavigation, createFooter } from "./components.js";
+import { myIntro } from "./textContent.js";
+import restaurant from "./restaurant.jpg";
 
 export default function createHome() {
   // Cache DOM
   const content = document.getElementById("content");
 
+  // contents
+  const homeContents = main(
+    { class: "main" },
+    div(
+      { class: "container" },
+      section(
+        { class: "home-content" },
+        p({ class: "intro" }, myIntro),
+        img({ src: restaurant, class: "restaurant-img" })
+      )
+    )
+  );
+
   // Render
   content.appendChild(createHeaderWithNavigation("رستوران من"));
-  content.appendChild(createMain());
+  content.appendChild(homeContents);
   content.appendChild(createFooter());
 }
