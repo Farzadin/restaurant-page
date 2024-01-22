b6841a45ec8e4b1da45cf80134c7ab71929fb129
Farzad DianatKhah <farzad.dianat@gmail.com>
2024-01-22 15:09:36 +0330

    Refactor components

--- src/components.js
+++ src/components.js
@@ -1,21 +1,4 @@
-import {
-  h1,
-  header,
-  div,
-  nav,
-  ul,
-  li,
-  a,
-  p,
-  footer,
-  i,
-  main,
-  section,
-  img,
-} from "./hypertext.js";
-
-import { myIntro } from "./textContent.js";
-import restaurant from "./restaurant.jpg";
+import { h1, header, div, nav, ul, li, a, p, footer, i } from "./hypertext.js";
 
 function createFooter() {
   const myFooter = footer(
@@ -62,43 +45,4 @@ function createHeaderWithNavigation(title) {
   return myHeader;
 }
 
-function createSectionForHome() {
-  const mySection = section(
-    { class: "section-home" },
-    p(null, myIntro),
-    img({ src: restaurant, class: "restaurant-img" })
-  );
-
-  return mySection;
-}
-
-function createSectionForContact() {
-  const mySection = section({ class: "section-contact" });
-
-  return mySection;
-}
-
-function createSectionForMenu() {
-  const mySection = section(
-    { class: "section-menu" },
-    p(null, "This is some random bullshit on Menu")
-  );
-
-  return mySection;
-}
-
-function createMain() {
-  const myMain = main(
-    { class: "main" },
-    div({ class: "container main-container" }, createSectionForHome())
-  );
-
-  return myMain;
-}
-export {
-  createFooter,
-  createHeaderWithNavigation,
-  createMain,
-  createSectionForHome,
-  createSectionForMenu,
-};
+export { createFooter, createHeaderWithNavigation };
