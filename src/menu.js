7449774aad6f0f5840284d60020f5b654b052c09
cyiran <58443122+thisiscyiran@users.noreply.github.com>
2024-01-23 19:57:17 +0330

    Add content to menu page

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,65 @@
-import { div, main, section } from "./hypertext.js";
+import { div, main, section, li, ul, p, h3, span, img } from "./hypertext.js";
 
 export default function createMenu() {
   const menuContents = main(
     { class: "main" },
-    div({ class: "container" }, section({ class: "menu-content" }))
+    div(
+      { class: "container" },
+      section(
+        { class: "menu-content" },
+        ul(
+          { class: "menu-list" },
+          li(
+            { class: "menu-item" },
+            img({ class: "menu-item-img" }),
+            div(
+              { class: "menu-item-text" },
+              h3(
+                { class: "menu-item-heading" },
+                span({ class: "menu-item-name" }, "خوراک کباب کوبیده"),
+                span({ class: "menu-item-price" }, "240T")
+              )
+            ),
+            p(
+              { class: "menu-item-desc" },
+              "کباب کوبیده یک غذای ایرانی محبوب است که از گوشت چرخ‌کرده، پیاز و ادویه‌های خاص تهیه می‌شود. این کباب به شکل استیک بلند و باریک درست و در نارنجی یا گاز پخته می‌شود."
+            )
+          ),
+          li(
+            { class: "menu-item" },
+            img({ class: "menu-item-img" }),
+            div(
+              { class: "menu-item-text" },
+              h3(
+                { class: "menu-item-heading" },
+                span({ class: "menu-item-name" }, "خوراک جوجه کباب"),
+                span({ class: "menu-item-price" }, "240T")
+              )
+            ),
+            p(
+              { class: "menu-item-desc" },
+              "جوجه کباب، گوشت مرغ با ادویه‌ها، بر روی سیخ پخته شده، طعمی خوشمزه و معطر از غذاهای محبوب ایرانی"
+            )
+          ),
+          li(
+            { class: "menu-item" },
+            img({ class: "menu-item-img" }),
+            div(
+              { class: "menu-item-text" },
+              h3(
+                { class: "menu-item-heading" },
+                span({ class: "menu-item-name" }, "خوراک کباب برگ"),
+                span({ class: "menu-item-price" }, "370T")
+              )
+            ),
+            p(
+              { class: "menu-item-desc" },
+              "کباب برگ، یک غذای ایرانی سنتی است که از تکه‌های نازک گوشت (معمولاً گوشت گوسفند یا گوساله)، به همراه ریزه‌های پنیر و ادویه‌های خاص، بین برگ‌های تره تهیه می‌شود. سپس بر روی آتش یا گاز پخته می‌شود"
+            )
+          )
+        )
+      )
+    )
   );
 
   return menuContents;
