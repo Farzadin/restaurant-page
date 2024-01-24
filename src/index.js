e01dc5cac60104b93924af3684971b8492744ebe
cyiran <58443122+thisiscyiran@users.noreply.github.com>
2024-01-24 17:37:07 +0330

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import createHome from "./homepage.js";
+import createHomePage from "./homepage.js";
 import createMenu from "./menu.js";
+import createContacts from "./contacts.js";
+import { createHomeContentWithoutMain } from "./components.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  createHome();
+  createHomePage();
 
-  const content = document.getElementById("content");
   const main = document.querySelector("main");
   const menu = document.querySelector(".menu");
+  const home = document.querySelector(".home");
+  const contact = document.querySelector(".contact");
+
+  function updateMainContent(content) {
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+
+    main.appendChild(content);
+  }
 
   menu.addEventListener("click", () => {
-    if (content.contains(main)) {
-      main.replaceWith(createMenu());
-    }
+    updateMainContent(createMenu());
+  });
+
+  home.addEventListener("click", () => {
+    updateMainContent(createHomeContentWithoutMain());
+  });
+
+  contact.addEventListener("click", () => {
+    updateMainContent(createContacts());
   });
 });
