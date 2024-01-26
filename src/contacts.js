a833850fd6c91f5a56678aae6060aec2f3437056
cyiran <58443122+thisiscyiran@users.noreply.github.com>
2024-01-27 02:38:19 +0330

    update contacts.js

--- src/contacts.js
+++ src/contacts.js
@@ -1,12 +1,15 @@
-import { div, p, h2 } from "./hypertext.js";
+import { div, p, h2, section } from "./hypertext.js";
 
 export default function createContacts() {
   const contactsContent = div(
     { class: "container" },
-    h2({ class: "contact-title" }, "تماس"),
-    p({ class: "manager-name" }, "ممد بوقی"),
-    p({ class: "manager-title" }, "مدیریت"),
-    p({ class: "manager-phone-number" }, "555-222-333")
+    section(
+      {},
+      h2({ class: "contact-title" }, "تماس"),
+      p({ class: "manager-name" }, "ممد بوقی"),
+      p({ class: "manager-title" }, "مدیریت"),
+      p({ class: "manager-phone-number" }, "555-222-333")
+    )
   );
 
   return contactsContent;
